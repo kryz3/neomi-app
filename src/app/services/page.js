8f9b5a773847e84d23eae7a03604a52796c748e6
kryz3 <147450400+kryz3@users.noreply.github.com>
2025-08-20 23:59:21 +0200

    services

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,15 +1,15 @@
+import ServicesComponent from "../components/Services";
+
+
 export const metadata = {
   title: "Blogs - Neomi",
   description: "Découvrez le blog de Neomi",
 };
 
 export default function Services() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Services</h1>
-      <p className="text-lg">
-        En chantier.
-      </p>
-    </div>
-  );
+  return(
+    <>
+    <ServicesComponent />
+    </>
+  )
 }
